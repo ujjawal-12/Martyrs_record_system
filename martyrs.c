09fb727a4d4c2fecaf7ec95778d33aa63501b1ce
ujjawa-12 <67854090+Ujjawal-kp@users.noreply.github.com>
2021-07-21 11:44:37 +0530

    commit

---
+++ martyrs.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+printf("THIS PROGRAM IS FOR STORING RECORD OF OUR MARTYRS.\n");
+printf("\n ENTER 1 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2010");
+printf("\n ENTER 2 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2011");
+printf("\n ENTER 3 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2012");
+printf("\n ENTER 4 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2013");
+printf("\n ENTER 5 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2014");
+printf("\n ENTER 6 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2015");
+printf("\n ENTER 7 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2016");
+printf("\n ENTER 8 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2017");
+printf("\n ENTER 9 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2018");
+printf("\n ENTER 10 FOR DETAILS OF SOLDIERS WHO LOST THEIR LIFE IN YEAR 2019");
+printf("\n ENTER ANY ONE OF THESE OPTIONS:- ");
+scanf("%d",&ch);
+printf("\n ***********************************************************************\n\n");
+switch(ch)
+ { case 1:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2010.\n");
+  printf("*********************************************************************\n");
+  printf("  NAME           REGIMENT          RANK      AGE      HONOURARY AWARDS\n");
+  printf("*********************************************************************\n");
+  printf("  Hemraj Meena   Rajputana Rifles  NK       20        N/A \n  ");
+  printf("Vijay Kumar    Dogra Regiment    LHAV     37        Sainya Seva Medal\n");
+  printf("--------------------------------------------------------------------------\n\n\n");
+  break;
+  case 2:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2011.\n");
+  printf("**********************************************************************\n");
+  printf("NAME           REGIMENT          RANK      AGE      HONOURARY AWARDS\n");
+  printf("**********************************************************************\n");
+  printf("Raman Thapa    Assam Regiment     SUB      41        N/A \n");
+  printf("Md. Ashraf     J&K Regiment       SUB      33        Sena Medal\n");
+  printf("------------------------------------------------------------------------\n\n\n");
+  break;
+  case 3:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2012.\n");
+  printf("*************************************************************************\n");
+  printf(" NAME           REGIMENT          RANK       AGE     HONOURARY AWARDS\n");
+  printf("*************************************************************************\n");
+  printf(" Jaimal Singh   Jatt Regiment     MAJ        46       Kirti Chakra\n ");
+  printf("Tilak Raj      Garhwal Regiment  Lt.        35       N/A \n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  case 4:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2013.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Vijay Soreng   Sikh Regiment      Capt.      41       N/A \n");
+  printf(" Sanjay Rajput  Rajputana Rifles   Col.       28       Vir Chakra\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  case 5:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2014.\n");
+  printf("************************************************************************\n");
+  printf("NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf("Mukhtar Malik  Dogra Regiment     NK         48       Shaurya Chakra\n");
+  printf("Hameer Singh   8th Lancers        Rfn.       32       N/A\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  case 6:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2015.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Vikram Singh   Mahrattas          MAJ        23      Sena Medal\n ");
+  printf("Kaustubh Rane  Parachute          MAJ        38      Vir Chakra\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  case 7:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2014.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Mandeep Rawat  Dogra Regiment     Rfn.       26       N/A\n");
+  printf(" Mukut Bihari   Madras Regiment    Ptr.       32       N/A\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;	
+  case 8:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2014.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Jaki Sharma    Thiyyar Regiment   Rfn.       19      Shaurya Chakra\n");
+  printf(" Shive Kumar    Kumaon Regiment    MAJ        43       N/A\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;	
+  case 9:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2014.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Nilesh Singh   Rajputana Regiment  Gnr.      47      Shaurya Chakra\n");
+  printf(" Iqbal Sheikh   Garhwal Regiment    Subedar   32       N/A\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  case 10:
+  printf("\n THESE SOLDIERS LOST THEIR LIFE IN YEAR 2014.\n");
+  printf("************************************************************************\n");
+  printf(" NAME           REGIMENT           RANK       AGE     HONOURARY AWARDS\n");
+  printf("************************************************************************\n");
+  printf(" Jorabar        Assam Regiment     Hav.       28      Vir Chakra\n");
+  printf(" Rakesh Singh   8th Lancers        NK         45      Kirti Chakra\n");
+  printf("-------------------------------------------------------------------------\n\n\n");
+  break;
+  
+  
+  default:
+  printf("PLEASE SELECT VALID OPERATION.\n");
+  printf("-------------------------------------------------------------------------\n\n");
+  }
+  printf("      WE\n");
+  printf(" LEAD BY EXAMPLE\n ");
+  printf("LIVE BY CHANCE\n");
+  printf(" LOVE BY CHOICE\n");
+  printf("     AND\n");
+  printf(" KILL BY PROFESSION\n");
+  printf("              -- INDIAN ARMY \n");
+  printf("  ** || JAI HIND || **\n");
+  printf("*************************************************************************\n");
+  getch();
+  }
+
